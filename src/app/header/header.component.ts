be96bf025b7c5c9fa28d32c558cb6c77325c39e2
jpca999 <jpca999@gmail.com>
2019-04-18 17:13:29 -0700

    added bootstrap

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+// @Component is a decorator and we'll pass a JS object to it.
+@Component({
+  selector:'app-header',
+  templateUrl:'./header.component.html',
+  
+})
+export class HeaderComponent{
+
+}
