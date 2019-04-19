87c2b014aa03bfb990936b24b7aa10310432d5f8
jpca999 <jpca999@gmail.com>
2019-04-18 17:22:56 -0700

    header compo added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+// import { AppHeader } from './header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    // AppHeader
   ],
   imports: [
     BrowserModule,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core'
 @Component({
   selector:'app-header',
   templateUrl:'./header.component.html',
-  
+
 })
 export class HeaderComponent{
 
